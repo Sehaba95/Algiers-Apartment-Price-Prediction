1ad55c0df86e9fd8c356efaaba503c558d20c509
Sehaba Amine <sehaba.amine2705@gmail.com>
2018-06-29 19:34:41 +0100

    Delete pictures and modify some scripts

--- Regressors/AdaBoostRegressor.py
+++ Regressors/AdaBoostRegressor.py
@@ -7,12 +7,12 @@ from sklearn.ensemble import AdaBoostRegressor
 
 data = pd.read_csv("dataset.csv",header=0)
 
-X = data.loc[:,["Commune","Etage","Superficie","Piece"]].values
+X = data.loc[:,["Commune","Etage"]].values
 Y = data.loc[:,"Prix"].values
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2)
 
-regressor = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),n_estimators=150)
+regressor = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),n_estimators=100)
 regressor.fit(X_train,Y_train)
 score = regressor.score(X_test,Y_test)
 print(score)
--- Regressors/RandomForestRegressor.py
+++ Regressors/RandomForestRegressor.py
@@ -6,12 +6,12 @@ from sklearn.ensemble import RandomForestRegressor
 
 data = pd.read_csv("dataset.csv",header=0)
 
-X = data.loc[:,["Commune","Etage","Superficie","Piece"]].values
+X = data.loc[:,["Commune","Etage"]].values
 Y = data.loc[:,"Prix"].values
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2)
 
-regressor = RandomForestRegressor(max_depth=5, random_state=0)
+regressor = RandomForestRegressor(max_depth=4, random_state=0)
 regressor.fit(X_train,Y_train)
 score = regressor.score(X_test,Y_test)
 print(score)
